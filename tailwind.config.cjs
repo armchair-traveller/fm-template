73d6902b8953302bff8dac5187f9b2b52c888d0a
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-04-16 20:17:43 -0700

    add comments, boilerplate for theme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,21 @@
 module.exports = {
-	mode: "jit",
-	purge: [
-		"./src/**/*.{html,js,svelte,ts}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  mode: "jit",
+  purge: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      // TODO: Your colors here
+      main: {
+        100: "#FFF",
+        200: "#FFF",
+        300: "#FFF",
+        400: "#FFF",
+      },
+    },
+    fontFamily: {
+      // TODO: Your fonts here
+      main: ["system-ui"],
+    },
+  },
+  plugins: [],
 };
