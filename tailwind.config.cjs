406537e1b9ce0a1c85f7dc9817439de156fc15cf
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-04-16 21:56:33 -0700

    move colors into extend

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,15 @@ module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
-    colors: {
-      // TODO: Your colors here
-      main: {
-        100: "#FFF",
-        200: "#FFF",
-        300: "#FFF",
-        400: "#FFF",
+    extend: {
+      colors: {
+        // TODO: Your colors here
+        main: {
+          100: "#FFF",
+          200: "#FFF",
+          300: "#FFF",
+          400: "#FFF",
+        },
       },
     },
     fontFamily: {
