721c9d8e449128be766c6a98c2fde53963fa7b27
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-04-20 17:37:02 -0700

    update import with example

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -6,7 +6,7 @@
 
 <style lang="postcss">
   /* TODO: Import fonts if needed */
-  /* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap'); */
+  /* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Orelega+One&display=swap'); */
   /* TODO: Set globals, CSS variables here */
 
   @layer base {
