4757b9109a97ceb6627c6aeecd151647cab46e4c
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-04-24 18:26:55 -0700

    update layouts

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -3,13 +3,3 @@
 </script>
 
 <slot />
-
-<style lang="postcss">
-  /* TODO: Import fonts if needed */
-  /* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Orelega+One&display=swap'); */
-  /* TODO: Set globals, CSS variables here */
-
-  @layer base {
-    font-family: system-ui;
-  }
-</style>
